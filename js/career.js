58259c2e7ad05758659a65bea74a6f6d72317ab0
dhanigill <132022244+dhanigill@users.noreply.github.com>
2023-10-22 15:00:27 -0500

    Add files via upload

--- js/career.js
+++ js/career.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
 // When the user clicks on <div>, open the popup
-var popup= document.getElementById("submit");
-function myfunction()  {
-  popup.innerHTML='SUBMITTED!';
+
+function check() {
+  const form = new FormData();
+  const firstName = form.get("fname");
+  console.log(firstName);
+  const lastName = form.get("lname");
+  console.log(lastName);
+  const email = form.get("email");
+  console.log(email);
+  const cellNumber = form.get("cnum");
+  console.log(cellNumber);
 }
-popup.addEventListener('click', myfunction);
 
 
 });
